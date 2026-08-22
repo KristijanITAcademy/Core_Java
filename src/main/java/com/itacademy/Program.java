03d6f90b29a104d389007e9a4824cfed568f6f3b
birkickristijan96-hash <birkickristijan96@gmail.com>
2026-08-22 13:43:18 +0200

    izmijenjen Program.java i dodan ThreeDemo

--- src/main/java/com/itacademy/Program.java
+++ src/main/java/com/itacademy/Program.java
@@ -3,5 +3,6 @@ package com.itacademy;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Pozdrav raja iz Intellij IDe");
+        System.out.println("Kako ste");
     }
 }
