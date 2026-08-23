d1fee96de1faccaae2b0e162acad7648498afc9a
birkickristijan96-hash <birkickristijan96@gmail.com>
2026-08-23 21:11:14 +0200

    Rezime\

---
+++ src/main/java/com/itacademy/five/FiveDemo.java
@@ -0,0 +1,20 @@
+package com.itacademy.five;
+
+/**
+ * varijabla je zapravo proizvoljni naziv memorijske lokacije
+ * TIP PODATKA VARIJABLA = VRIJEDNOST
+ * Tipovi podataka:
+ *  prosti
+ *       - cijeli brojevi - byte,short, int,long
+ *       - decimalni - float, double
+ *       - char ' '
+ *       - boolean - true false
+ *
+ *  slozeni tj objektni
+ */
+public class FiveDemo {
+    public static void main(String[] args) {
+
+
+    }
+}
